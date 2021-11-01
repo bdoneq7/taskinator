d44a756e11e9a96d029ea76f40864e1b356d586a
Brian Done <briguy2078@gmail.com>
2021-11-01 14:08:15 -0400

    changed event listener to form instead of just button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-// When Add Task Button is Clicked, Add new Task Item
-var createTaskHandler = function() {
+// When Add Task Button is Clicked in the Form, Add new Task Item
+var createTaskHandler = function(event) {
+  event.preventDefault();      
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
 };
 
-// Listen for Add Task Button Click by User
-buttonEl.addEventListener("click", createTaskHandler);
+// Listen for Add Task Button Click in the Form by User and Run Task Function
+formEl.addEventListener("submit", createTaskHandler);
